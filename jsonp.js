58609f1616b775b6e8b01f789c6d7430d73bcf25
Keifer Gu <keifergu@gmail.com>
2017-10-13 18:11:33 +0800

    feat(jsonp): 添加跨域

--- jsonp.js
+++ jsonp.js
@@ -2,15 +2,24 @@
   var jsonp = {};
 
   function callbackWrapper(callback) {
-    return function(data) {
+    return function (data) {
       callback(data)
     }
   }
+
   jsonp.get = function get(src, callback) {
     var script = document.createElement("script")
+    script.onload = function () {
+      script.parentNode.removeChild(script)
+    }
+    script.onerror = function () {
+      
+    }
     script.src = src
     window.callback = callbackWrapper(callback)
     document.body.appendChild(script)
   }
+
   global.jsonp = jsonp
+
 })(window)

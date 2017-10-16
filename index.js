917eec892fd272dc057fea83f3f18940817ac952
Keifer Gu <keifergu@gmail.com>
2017-10-16 18:51:45 +0800

    feat(acao): 添加跨域接口

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ app.use( async ( ctx ) => {
     ctx.body = jsonpWrapper('callback', {name: 'wanghong'})
     ctx.set("Content-Type", 'application/javascript; charset=UTF-8')
     break
+  case 'acao':
+    ctx.set("Access-Control-Allow-Origin", "http://localhost:8000")
+    ctx.body = JSON.stringify(ctx.request)
+    break
   default:
     ctx.set("Content-Type", 'text/html; charset=UTF-8')
+    ctx.set('Cache-Control', 'no-cache');
+    ctx.set("Set-Cookie", "username=wanghong")
     ctx.body = fs.readFileSync(url)
   }
 })
